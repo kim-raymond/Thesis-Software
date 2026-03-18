d859bc0552ca2d3036b8bab67ee6b1a95ae79d3a
kim-raymond <magallanesraymond27@gmail.com>
2026-03-18 14:57:01 +0800

    calib

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,7 @@
 
 import Login from "@/components/Login";
 import Main from "@/components/Main";
-import Test from "@/components/test";
 import { useState } from "react";
-// import Test from "@/components/test";
 export default function Home() {
 
   const [logIn, setLogIn] = useState<boolean>(false);
@@ -12,7 +10,6 @@ export default function Home() {
     <div>
     <Login logIn={logIn} setLogIn={setLogIn}/>
     <Main logIn={logIn} setLogIn={setLogIn}/>
-    <Test />
     </div>
   );
 }
