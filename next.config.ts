c32e3073549ac977fb0126e471cadb32c663d57c
kim-raymond <magallanesraymond27@gmail.com>
2026-04-19 17:48:13 +0800

    fixBuild

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* You can put other Next.js options here if you have them */
-  
+
   webpack: (config, { isServer }) => {
     // This part fixes the 'fs' error for your Random Forest library
     if (!isServer) {
